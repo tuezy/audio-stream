2bd763267ff3e35e634d8e9f362d5d970b2e277f
hoangtu <hoangtu@ohlaalaa.com>
2023-03-07 08:05:42 +0700

    add remove element

--- resources/views/index/pages/home.blade.php
+++ resources/views/index/pages/home.blade.php
@@ -72,30 +72,6 @@
 
     <script>
 
-        const swiper = new Swiper('.swiper', {
-            // Optional parameters
-            slidesPerView: 10,
-            centeredSlides: true,
-            spaceBetween: 10,
-            loop: true,
-
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
-
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-
-            // And if we need scrollbar
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        });
-
         var player = new Playerjs({id:"player", file:[
             ]});
 
