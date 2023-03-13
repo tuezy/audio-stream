ec49af237ee42f490a5ca9532962d12250c1aac6
hoangtu <hoangtu@ohlaalaa.com>
2023-03-13 18:39:38 +0700

    fix search

--- resources/views/index/pages/home.blade.php
+++ resources/views/index/pages/home.blade.php
@@ -66,7 +66,7 @@
 @endsection
 
 @push("scripts")
-    <script src="{{ asset("assets/libs/playerjs_audio_2.js") }}"></script>
+    <script src="{{ asset("assets/libs/playerjs_audio_2.js?v=1") }}"></script>
     <script
             src="https://code.jquery.com/jquery-3.6.4.min.js"
             integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
