e305bf5a87015cf78ec28fa7e39f19195ac38102
hoangtu <hoangtu@ohlaalaa.com>
2023-03-13 18:01:31 +0700

    fix search

--- resources/views/index/pages/search/index.blade.php
+++ resources/views/index/pages/search/index.blade.php
@@ -5,46 +5,64 @@
         <div id="mediaplay" class="py-5">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-8">
+                    <div class="col-lg-6">
                         <div class="media-player">
                             <div id="player"></div>
-                            <div class="playing">
-                                <div class="playing__film">
-                                    {{ $video->title ?? '' }}
-                                </div>
-                            </div>
-                            <div id="playlist">
-                                @foreach($items as $item)
-                                    <div class="media-item media-item__{{$item->id}}">
-                                        <div class="media-item__icon" href="{{ $item->path }}" target="media-item__{{$item->id}}">
-                                        </div>
-                                        <div class="media-item__info">
-                                            <div>
-                                                <div class="title">{{ $item->title }}</div>
-                                                <div class="description">{{ \Illuminate\Support\Str::words($item->content, 10) }}</div>
+                        </div>
+                        <div class="media-form mt-3">
+                            <livewire:link-status></livewire:link-status>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="panel-danhsachphat">
+                            <span class="title">Danh sách phát</span>
+                            <a class="nav-link" data-bs-toggle="modal" id="create-btn-search" data-bs-target="#showModal"><span class="btn-taomoi">Tạo mới</span></a>
+                        </div>
+                        <div class="playing">
+                            <div class="playing__film">
+                                <div class="slick-wrapper">
+                                    @foreach($playlists as $broadcast_date => $playlist)
+                                        <div>
+                                            <div class="broadcast_date broadcast_date_{{$playlist[0]['id']}}"
+                                                 value="{{$playlist[0]['id']}}"
+                                                 onclick="changePlaylist({{$playlist[0]['id']}}, 'broadcast_date_{{$playlist[0]['id']}}')">
+                                                {{ \Illuminate\Support\Carbon::createFromFormat("Y-m-d", $broadcast_date)->format("d-m-Y") }}
                                             </div>
-
                                         </div>
-                                        <div class="media-item__download">
-                                            <a href="{{ $item->path }}" download="{{ $item->slug }}" class="media-item__downloadPath" target="_blank" title="{{ $item->title }}">
-                                                <img src="{{ asset("images/icon_download.png") }}" alt="icon_download"> Tải xuống
-                                            </a>
-                                        </div>
-                                        <div class="media-item__delete">
-                                            <a onclick="singleDelete({{ $item->id }})">
-                                                <img src="{{ asset("images/icon_delete.png") }}" alt="icon_delete"> Xóa
-                                            </a>
-                                        </div>
-                                    </div>
-                                @endforeach
-                                <div class="my-3">
-                                    {{ $items->links() }}
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4">
-                        @include("index.pages.ui.search")
+                        <div id="playlist">
+                            @foreach($items as $item)
+                                <div class="media-item media-item__{{$item->id}}" value="{{$item->id}}">
+                                    <div class="media-item__icon" onclick="audioplay('{{ asset($item->path) }}', '{{ $item->title }}')" target="media-item__{{$item->id}}">
+                                    </div>
+                                    <div class="media-item__info">
+                                        <div>
+                                            <div class="title">{{ $item->title }}</div>
+                                            <div class="description">{{ \Illuminate\Support\Str::words($item->content, 10) }}</div>
+                                        </div>
+
+                                    </div>
+                                    <div class="media-item__download">
+                                        <a href="{{ $item->path }}" download="{{ $item->slug }}" class="media-item__downloadPath" target="_blank">
+                                            <img src="{{ asset("images/icon_download.png") }}" alt="icon_download"> Tải xuống
+                                        </a>
+                                    </div>
+                                    <div class="media-item__delete">
+                                        <a onclick="singleDelete({{ $item->id }})">
+                                            <img src="{{ asset("images/icon_delete.png") }}" alt="icon_delete"> Xóa
+                                        </a>
+                                    </div>
+                                </div>
+                            @endforeach
+                            <div class="my-3">
+                                {{ $items->links() }}
+                            </div>
+                        </div>
+                        <button class="my-3 btn btn-outline-success btn-update-index-playlist d-none" onclick="updateIndexPlaylist()">Cập nhật Playlist</button>
+
                     </div>
                 </div>
             </div>
@@ -58,22 +76,12 @@
                     <div class="row">
                         @isset($cms)
                             @foreach($cms as $item)
-                                <div class="col-lg-6">
-                                    <div class="cms-item mb-4">
-                                        <div class="title">
-                                            {{ $item->title }}
-                                            <div class="btn-more-pos"><span class="btn-more"></span></div>
-                                        </div>
-                                        <div class="content mt-2">
-                                            {{ $item->content }}
-                                        </div>
-                                    </div>
-                                </div>
+                                @include('index.pages.cms.partials.item', ['item' => $item])
                             @endforeach
                         @endisset
                     </div>
                     <div class="row">
-                        <button class="btn-xemthem mx-auto">XEM THÊM</button>
+                        <a class="btn-xemthem mx-auto" href="{{ route("index.faq") }}">XEM THÊM</a>
                     </div>
 
                 </div>
@@ -84,41 +92,39 @@
 @endsection
 
 @push("scripts")
-    <script src="{{ asset("assets/libs/playerjs.js") }}"></script>
+    <script src="{{ asset("assets/libs/playerjs_audio_2.js") }}"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8="
+            crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script>
-        var cmsItem = document.getElementsByClassName("cms-item");
-        Array.from(cmsItem).forEach(function (element) {
-            element.addEventListener("click", function () {
-                this.classList.toggle("active");
-            });
+        $('.slick-wrapper').slick({
+            dots: false,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 5,
+            slidesToScroll: 1,
         });
-    </script>
-    <script>
-        var player = null;
-        // var mediaItems = document.getElementsByClassName("media-item");
-        // Array.from(mediaItems).forEach(function (element) {
-        //     element.addEventListener("click", function () {
-        //         const inner = element.getElementsByClassName('media-item__downloadPath');
-        //         const href = inner[0].href;
-        //
-        //         var mediaSelecting = document.getElementsByClassName("media_selected");
-        //         mediaSelecting[0].classList.remove("media_selected");
-        //         element.classList.add("media_selected");
-        //         player.api("play", href);
-        //     });
-        // });
-        //
-        // if(mediaItems.length > 0){
-        //     var firstItem = mediaItems[0].getElementsByClassName('media-item__downloadPath');
-        //     var firstItemHref = firstItem[0].href;
-        //     player = new Playerjs({id:"player", file:firstItemHref});
-        //     mediaItems[0].classList.add('media_selected');
-        // }
+        var player = new Playerjs({id:"player", file:[
+            ],
+            poster: "{{asset("images/play-final.png")}}"
+        });
+
+        var playlists = document.getElementsByClassName("broadcast_date");
+
+        if(playlists.length > 0){
+            const firstPlaylist = playlists[0].getAttribute("value");
+            playlists[0].classList.add('active');
+            loadPlaylist(firstPlaylist);
+        }else{
+            console.log("Empty playlist");
+        }
 
         var mediaVideos = document.getElementsByClassName("media-item__icon");
+
         Array.from(mediaVideos).forEach(function (element) {
             element.addEventListener("click", function () {
-                const href = element.getAttribute("href");
                 var mediaSelected = element.getAttribute("target");
 
                 var mediaSelecting = document.getElementsByClassName("media_selected");
@@ -127,51 +133,84 @@
                 }
 
                 var videoSelected = document.getElementsByClassName(mediaSelected);
-
                 videoSelected[0].classList.add("media_selected");
-                player.api("play", href);
             });
         });
 
-        if(mediaVideos.length > 0){
-            const href = mediaVideos[0].getAttribute("href");
-            player = new Playerjs({id:"player", file:href});
+        function changePlaylist(id,event){
+            loadPlaylist(id);
+            let broadcastDate = document.getElementsByClassName("broadcast_date");
+            Array.from(broadcastDate).forEach(function (element) {
+                if(element.classList.contains('active')){
+                    element.classList.remove('active');
+                }
+                //
+            });
+            let broadcastDateSeletcted = document.getElementsByClassName(event);
+            broadcastDateSeletcted[0].classList.add('active');
+        }
 
-            var mediaSelected = element.getAttribute("target");
-
-            var mediaSelecting = document.getElementsByClassName("media_selected");
-            if(mediaSelecting.length > 0){
-                mediaSelecting[0].classList.remove("media_selected");
+        function loadPlaylist(id){
+            try {
+                axios.post('{{request()->url()}}/playlist', {
+                    data: {
+                        id: id
+                    }
+                }).then(function (response) {
+                    document.getElementById('playlist').innerHTML = response.data;
+                })
+            } catch (error) {
+                console.log(error)
             }
+            updatePlayer();
+        }
 
-        }else {
-            player = new Playerjs({id:"player"});
+        function audioplay(link, title){
+            player.api("play", link);
+            player.api("title", title);
+        }
+
+        function updatePlayer(){
+            var downloadPaths = document.getElementsByClassName("media-item__downloadPath");
+            player.api("playlist", []);
+            Array.from(downloadPaths).forEach(function (element) {
+                let aHerf = element.getAttribute("href");
+                let aTitle = element.getAttribute("title");
+                player.api("push", [{
+                    "file" : aHerf,
+                    "title" : aTitle
+                }])
+            });
         }
 
 
     </script>
+
     <script>
         function singleDelete(id){
             Swal.fire({
-                title: "Are you sure?",
-                text: "You won't be able to revert this!",
+                title: "Bạn có chắc muốn xóa?",
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonClass: 'btn btn-primary w-xs me-2 mt-2',
                 cancelButtonClass: 'btn btn-danger w-xs mt-2',
-                confirmButtonText: "Yes, delete it!",
+                confirmButtonText: "Có",
+                cancelButtonText: "Không",
                 buttonsStyling: false,
                 showCloseButton: true
             }).then(function (result) {
                 if (result.value) {
                     try {
-                        axios.delete('{{route("video.delete")}}', {
+                        axios.delete('{{route("customers.delete.audio")}}', {
                             data: {
-                                ids: id
+                                id: id
                             }
                         }).then(function (response) {
                             if(response.data.success){
-                                location.reload();
+                                // location.reload();
+                                let target = "media-item__"+id;
+                                let element = document.getElementsByClassName(target);
+                                element[0].remove();
                             }
                         })
                     } catch (error) {
@@ -180,6 +219,87 @@
                 }
             });
         }
-    </script>
 
+        var drake = dragula([document.getElementById("playlist")])
+            .on('drag', function (el) {
+                el.className = el.className.replace('ex-moved', '');
+            }).on('drop', function (el) {
+                el.className += ' ex-moved';
+            }).on('over', function (el, container) {
+                container.className += ' ex-over';
+            }).on('out', function (el, container) {
+                container.className = container.className.replace('ex-over', '');
+                let btn = document.getElementsByClassName("btn-update-index-playlist");
+                btn[0].classList.remove('d-none');
+            });
+
+        function updateIndexPlaylist(){
+            var playlists = document.getElementsByClassName("media-item");
+            var indexs = [];
+
+            Array.from(playlists).forEach(function (element) {
+                indexs.push(element.getAttribute("value"));
+            });
+
+            try {
+                axios.post('{{route("customers.update.playlist")}}', {
+                    data: {
+                        indexs: JSON.stringify(indexs)
+                    }
+                }).then(function (response) {
+                    if(response.data.success){
+                        location.reload();
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    </script>
+@endpush
+
+@push("styles")
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <style>
+        #player {
+            height: 320px!important;
+        }
+        .slick-prev,.slick-next{
+            position: absolute;
+            width: 40px;
+            height: 40px;
+            background: #fff;
+            color: transparent;
+            overflow: hidden;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+            z-index: 1;
+        }
+        .slick-prev{
+            left: -35px;
+        }
+        .slick-next{
+            right: -35px;
+        }
+        .slick-prev:before{
+            content: "\ea64";
+            font-family: remixicon!important;
+            font-style: normal;
+            -webkit-font-smoothing: antialiased;
+            font-size:20px;
+            color: #000;
+        }
+        .slick-next:before{
+            content: "\ea6e";
+            font-family: remixicon!important;
+            font-style: normal;
+            -webkit-font-smoothing: antialiased;
+            font-size:20px;
+            color: #000;
+        }
+        .broadcast_date{
+            cursor:pointer;
+        }
+    </style>
 @endpush
