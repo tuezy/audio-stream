cadee6abf30f759367a92895459e10ec9db5e9a5
hoangtu <hoangtu@ohlaalaa.com>
2023-03-15 16:11:27 +0700

    abc

--- resources/views/index/layouts/partials/footer.blade.php
+++ resources/views/index/layouts/partials/footer.blade.php
@@ -1,11 +1,16 @@
 <footer class="custom-footer position-relative d-flex align-items-center">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center">
-            <div>
-                <p class="copy-rights mb-0">
-                    {{ core()->getSetting('site_title') }} | Copyright © <script> document.write(new Date().getFullYear()) </script>
+
+                <div class="copy-rights mb-0">
+                <div>
+                    <img src="{{ asset("images/play-final.png") }}" alt="logo">
+                </div>
+                <p>Mã số thuế: 03055633881</p>
+                <p>Địa chỉ: {{ core()->getSetting('address') }}</p>
+                <p>© Bản quyền thuộc về Hoàng Thế Long</p>
+{{--                    {{ core()->getSetting('site_title') }} | Copyright © <script> document.write(new Date().getFullYear()) </script>--}}
                 </p>
-            </div>
             <div>
                 Hỗ trợ: {{ core()->getSetting('hotline') }}
             </div>
