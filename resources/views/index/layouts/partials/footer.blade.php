7e053ae4591c04724e454e8b3e853f6c08f69a81
hoangtu <hoangtu@ohlaalaa.com>
2023-03-15 21:14:20 +0700

    commit

--- resources/views/index/layouts/partials/footer.blade.php
+++ resources/views/index/layouts/partials/footer.blade.php
@@ -1,8 +1,8 @@
-<footer class="custom-footer position-relative d-flex align-items-center" >
+<footer class="custom-footer position-relative d-flex align-items-center" style="height: 100px">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center">
             <div>
-                <div class="copy-rights mb-0">
+                <div class="copy-rights mb-0" style="font-size: 14px">
                     <p>Mã số thuế: 03055633881</p>
                     <p>Địa chỉ: {{ core()->getSetting('address') }}</p>
                     <p>© Bản quyền thuộc về Hoàng Thế Long</p>
