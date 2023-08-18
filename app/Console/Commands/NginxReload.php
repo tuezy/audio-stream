45d13b5e3dd7e82a12b45181893483c661cdff76
hoangtu <vohoangtu0409@gmail.com>
2023-08-18 15:03:38 +0700

    run cmd in queue

--- app/Console/Commands/NginxReload.php
+++ app/Console/Commands/NginxReload.php
@@ -2,12 +2,18 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-class NginxReload extends Command
+class NginxReload extends Command implements ShouldQueue
 {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     /**
      * The name and signature of the console command.
      *
