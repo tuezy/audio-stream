644d5821e3be2a83949a0da503e69764e16b352e
hoangtu <vohoangtu0409@gmail.com>
2023-08-19 09:07:07 +0700

    fix link menu

--- resources/views/dashboard/layouts/partials/sidebar.blade.php
+++ resources/views/dashboard/layouts/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- LOGO -->
     <div class="navbar-brand-box">
         <!-- Dark Logo-->
-        <a href="index.html" class="logo logo-dark">
+        <a href="" class="logo logo-dark">
             <span class="logo-sm">
                 <img src="{{ asset("assets/images/logo-broadcast.png") }}" alt="" height="22">
             </span>
@@ -12,7 +12,7 @@
             </span>
         </a>
         <!-- Light Logo-->
-        <a href="index.html" class="logo logo-light">
+        <a href="" class="logo logo-light">
             <span class="logo-sm">
                 <img src="{{ asset("assets/images/logo-broadcast.png") }}" alt="" height="22">
             </span>
